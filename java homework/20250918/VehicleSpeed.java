f1848aa1c236d0dcdc867def6f70e1ed7158dff2
cutehumanS2 <oddpinksyrup@gmail.com>
2025-09-21 01:18:47 +0900

    250918 과제 리뷰 - 최가희

--- java homework/20250918/VehicleSpeed.java
+++ java homework/20250918/VehicleSpeed.java
@@ -3,11 +3,9 @@ package ch4;
 public class VehicleSpeed {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Vehicle z1 = new Car(60);
 		Vehicle z2 = new Bicycle(50);
-		
 		
 		z1.move();
 		z2.move();
