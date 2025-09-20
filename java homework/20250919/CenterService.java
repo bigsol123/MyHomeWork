34086efc6f44ed3ce10c05fbbd7cdf89ab0e7fb4
cutehumanS2 <oddpinksyrup@gmail.com>
2025-09-20 20:53:58 +0900

    250919 과제 리뷰 - 최가희

--- java homework/20250919/CenterService.java
+++ java homework/20250919/CenterService.java
@@ -10,6 +10,12 @@ public class CenterService {
 		this.dao = dao;
 	}
 
+	/* 🎀
+	* 여기 메서드명도 조금 더 생각해 보면, 우리가 저장하려는 대상이 센터일까요, 회원일까요?😸
+	* 만약 회원이라면, 어떻게 짓는 게 좀 더 명확할까요?!
+	* 메서드명, 변수명, 클래스명 모두 이런 방식으로 생각해 주시면, 더 명확한 네이밍을 지을 수 있게 될 것 같아요.
+	* 저도 이 부분은 아직 미숙해서 노력하고 있는 중입니다ㅎㅎ
+	*/
 	public void addCenter(String name, int number, int skcal, int ckcal) {
 		CenterDTO dto = new CenterDTO(name, number, skcal, ckcal);
 		dao.save(dto);
--- java homework/20250919/FoodDTO.java
+++ java homework/20250919/FoodDTO.java
@@ -4,7 +4,11 @@ public class FoodDTO {
 
 	private int number;
 	private String name;
+	// 🎀 변수명은 보통 명사형으로 작성하는 게 좋아요! ex) count, quantity, amount ...
 	private int many;
+	// 🎀 자바에 날짜형을 취급하는 자료형이 있다는 거 아시나요?! Date, LocalDate 등등
+	// 나중에는 이 자료형도 활용해 보는 것도 좋을 것 같습니다~
+	// 그리고 확실하진 않지만, 날짜 자료형을 안 쓰면 보통 날짜는 String으로 처리했던 것 같아요! 참고만 해 주세요.
 	private int date;
 
 	public FoodDTO(int number, String name, int many, int date) {
